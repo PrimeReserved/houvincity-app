200209c02df5aa71962c7bf12f06733c00786aa4
Archibong Samuel <141365223+archbong@users.noreply.github.com>
2024-04-02 12:01:16 +0100

    Revert "Feature/frontend UI update"

--- components/Blog/Cards/BlogCard.tsx
+++ components/Blog/Cards/BlogCard.tsx
@@ -1,7 +1,7 @@
 // pages/index.tsx
 
 import React from "react";
-import imageUrlBuilder from '@sanity/image-url'
+import imageUrlBuilder from "@sanity/image-url";
 import Calendar from "@/public/images/blog/Calendar.svg";
 import ArrowRightWhite from "@/public/images/blog/ArrowRightWhite.svg";
 import Image from "next/image";
@@ -72,8 +72,6 @@ const BlogCard = ({ posts }: { posts: SanityDocument[] }) => {
     </div>
   );
 };
-
-
 export default BlogCard;
 
 
