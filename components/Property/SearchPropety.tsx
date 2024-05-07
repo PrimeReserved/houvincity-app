fb30221d11c45228664ffb7acfa6efb5d6e8f44a
CallMePat <patienceemaiku@gmail.com>
2024-05-07 13:13:07 +0100

    Adjusted UI

--- components/Property/SearchPropety.tsx
+++ components/Property/SearchPropety.tsx
@@ -129,8 +129,8 @@ const SearchProperty = () => {
 
 
   return (
-    <div className="flex flex-col justify-center items-center -mt-10 ">
-      <div className="xl:w-[1019px] lg:w-[800px] md:w-[520px]  lg:h-[117px] md:h-[90px] h-[80px] bg-white shadow-lg rounded-lg flex items-center justify-evenly gap-2 px-2">
+    <div className="flex flex-col justify-center items-center -mt-10 mx-2">
+      <div className="w-[100%] md:w-[80%] lg:h-[117px] md:h-[90px] h-[80px] bg-white shadow-lg rounded-lg flex items-center justify-evenly gap-2 px-2 pr-5">
         <div className="relative">
           <select
             className="select select-bordered mx-2 py-2 w-full max-w-xs"
@@ -192,7 +192,7 @@ const SearchProperty = () => {
 
       </div>
 
-      <button className="flex gap-3 bg-primary hover:bg-primary/80 duration-300 ease-in- xl:w-[175px] md:w-[120px]  w-[100px] xl:h-[50px] md:h-[40px] h-[35px] rounded-lg items-center justify-center -mt-4 xl:-mt-6 md:-mt-5 " onClick={applyFilter}>
+      <button className="flex gap-3 bg-primary hover:bg-primary/80 duration-300 ease-in- xl:w-[175px] md:w-[120px]  w-[100px] xl:h-[50px] md:h-[40px] h-[35px] rounded-lg items-center justify-center -mt-3 xl:-mt-6 md:-mt-3 lg:-mt-5" onClick={applyFilter}>
         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 16 16">
           <path fill="#fff" d="M11.063 9.75h-.692l-.245-.236A5.662 5.662 0 0011.5 5.813 5.687 5.687 0 105.812 11.5a5.662 5.662 0 003.702-1.374l.236.245v.691l4.375 4.367 1.304-1.304-4.367-4.375zm-5.25 0a3.932 3.932 0 01-3.938-3.938 3.932 3.932 0 013.938-3.937A3.932 3.932 0 019.75 5.813 3.932 3.932 0 015.812 9.75z"></path>
         </svg>
