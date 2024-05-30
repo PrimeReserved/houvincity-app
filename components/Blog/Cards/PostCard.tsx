120a5167ab96a2edda105276efc72e8236bdab4a
archbong <archibong.chinonso.samuel@gmail.com>
2024-05-30 16:19:43 +0100

    Resolved images sizes in blog, news, and property

--- components/Blog/Cards/PostCard.tsx
+++ components/Blog/Cards/PostCard.tsx
@@ -1,5 +1,5 @@
 // pages/index.tsx
-"use client"
+"use client";
 
 import Calendar from "@/public/images/blog/Calendar.svg";
 import ArrowRightWhite from "@/public/images/blog/ArrowRightWhite.svg";
@@ -10,9 +10,6 @@ import { Suspense } from "react";
 import { usePathname } from "next/navigation";
 import Loading from "@/app/loading";
 import { urlForImage } from "@/sanity/lib/image";
-
-
-
 
 export default function PostCard({ post }: Readonly<any>) {
   const pathName = usePathname();
@@ -29,7 +26,7 @@ export default function PostCard({ post }: Readonly<any>) {
   } else if (isNewsPage) {
     basePath = "/news";
   } else if (isHomePage) {
-    basePath = "/blog"
+    basePath = "/blog";
   }
 
   if (!post) {
@@ -37,12 +34,22 @@ export default function PostCard({ post }: Readonly<any>) {
   }
   return (
     <div className="mt-5">
-        <div
-          key={post._id}
-          className="border rounded-lg overflow-hidden bg-white shadow-md  transition duration-300 transform hover:scale-105 flex flex-col"
-        >
-          <div className="relative overflow-hidden rounded-t-lg h-100">
-            <Suspense fallback={<Loading />}>
+      <div
+        key={post._id}
+        className="border rounded-lg overflow-hidden bg-white shadow-md  transition duration-300 transform hover:scale-105 flex flex-col"
+      >
+        <div className="relative overflow-hidden rounded-t-lg h-100">
+          <Suspense fallback={<Loading />}>
+            <div
+              style={{
+                width: "100%",
+                maxWidth: "380px",
+                height: "auto",
+                aspectRatio: "380 / 280",
+                overflow: "hidden",
+                position: "relative",
+              }}
+            >
               <Image
                 src={urlForImage(post?.mainImage)}
                 alt={`${post.slug?.current}`}
@@ -51,45 +58,45 @@ export default function PostCard({ post }: Readonly<any>) {
                 loading="lazy"
                 className="w-full h-full object-cover"
               />
-            </Suspense>
-          </div>
-          <div className="card-body items-start mt-1 ">
-            <div className="flex items-center gap-2">
-              <Image src={Calendar} alt="Calendar" width={13} height={13} />
-              {new Date(post.publishedAt).toLocaleDateString("en-US", {
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-              })}
             </div>
+          </Suspense>
+        </div>
+        <div className="card-body items-start mt-1 ">
+          <div className="flex items-center gap-2">
+            <Image src={Calendar} alt="Calendar" width={13} height={13} />
+            {new Date(post.publishedAt).toLocaleDateString("en-US", {
+              day: "numeric",
+              month: "long",
+              year: "numeric",
+            })}
+          </div>
 
-            <h1 className="font-semibold text-lg mt-1">{post?.title}</h1>
-           {/* Render description only if not on the home page */}
+          <h1 className="font-semibold text-lg mt-1 line-clamp-1">{post?.title}</h1>
+          {/* Render description only if not on the home page */}
           {!isHomePage && (
             <p className="line-clamp-3 text-[12px]">
               {post?.body?.map((block: any) => (
                 <span key={block._key}>
-                  {
-                    block?.children?.map((child: any) => child.text)}
+                  {block?.children?.map((child: any) => child.text)}
                 </span>
               ))}
             </p>
           )}
-            <div className="card-actions">
-              <Link href={`${basePath}/${post?.slug?.current || ''}`}>
-                  <button className="btn bg-primary text-white text-xs rounded-md hover:text-primary hover:bg-white hover:border-[1px] hover:border-primary flex gap-3 items-center">
-                    Read more
-                    <Image
-                      src={ArrowRightWhite}
-                      alt="Arrow Right"
-                      width={12}
-                      height={12}
-                    />
-                  </button>
-                </Link>
-            </div>
+          <div className="card-actions">
+            <Link href={`${basePath}/${post?.slug?.current || ""}`}>
+              <button className="btn bg-primary text-white text-xs rounded-md hover:text-primary hover:bg-white hover:border-[1px] hover:border-primary flex gap-3 items-center">
+                Read more
+                <Image
+                  src={ArrowRightWhite}
+                  alt="Arrow Right"
+                  width={12}
+                  height={12}
+                />
+              </button>
+            </Link>
           </div>
         </div>
+      </div>
       {/* <NumberCount
         totalPages={totalPages}
         currentPage={currentPage}
