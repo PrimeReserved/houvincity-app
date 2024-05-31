d62c5a56fff244cd79d90c3cbd81cfa9a577544e
archbong <archibong.chinonso.samuel@gmail.com>
2024-05-31 02:06:30 +0100

    Started working on liveOpen component

--- components/LandingPage/liveOpen.tsx
+++ components/LandingPage/liveOpen.tsx
@@ -1,14 +1,21 @@
 export default function LiveOpen(){
 
     return (
-        <div>
+        <div
+        style={{
+            backgroundImage: 'url(`https://res.cloudinary.com/dzd51q99i/image/upload/v1717116138/houvincity/landing-page/Rectangle_23893_1_dgd7ig.png`)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+          >
             <div>
                 <h1 className="text-customSecondary text-3xl font-semibold my-[7rem] flex justify-center">View Live Open Houses</h1>
             </div>
             <div className="h-96 w-96">
-                <h1>Join our live video tours and<br /> check upcoming events</h1>
-
-                <div>
+                <div className="m-3">
+                    <h1>Join our live video tours and<br /> check upcoming events</h1>
+                </div>
+                <div className="m-5">
                     <button className="btn bg-primary">View</button>
                 </div>
             </div>
